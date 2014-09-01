0061e5136bf81ff6ae59c6ff5091c5d35d020443
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2014-09-01 15:11:00 +0200

    Update redirect.js

--- src/assets/js/redirect.js
+++ src/assets/js/redirect.js
@@ -11,7 +11,7 @@ define(function(require, exports, module) {
     // your app here
     var logo = new ImageSurface({
         size: [200, 200],
-        content: 'http://code.famo.us/assets/famous_logo.svg',
+        content: 'https://raw.githubusercontent.com/iSC-Host/jdsf.isc/gh-pages/src/assets/img/redirect/gear.png',
         classes: ['double-sided']
     });
 
