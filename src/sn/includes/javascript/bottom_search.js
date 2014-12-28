e67766a5bbff85e2611aefe55e522979e24543df
Suriyaa Kudo <isc.suriyaa@gmail.com>
2014-12-28 12:23:30 +0100

    Delete sn folder (2)

--- src/sn/includes/javascript/bottom_search.js
+++
@@ -1,3 +0,0 @@
-jQuery(function($){
-$("#searchbox").Watermark("{-$search_member}");
-});
--- src/sn/includes/templates_compiled/%%56^564^564F3554%%start.tpl.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2014-02-07 15:21:37
-         compiled from file:style/newcunity/templates/start.tpl.html */ ?>
-<div>
-    <?php echo $this->_tpl_vars['BODY']; ?>
-
-</div>
